9330280d83b2b41314a62035d794d27ff1c5083e
debussy2k <harry@motion1.co.kr>
2019-04-12 23:45:43 +0900

    build dist

--- dist/opentype.js
+++ dist/opentype.js
@@ -11414,7 +11414,8 @@ function Font(options) {
  * @return {Boolean}
  */
 Font.prototype.hasChar = function(c) {
-    return this.encoding.charToGlyphIndex(c) !== 0;
+    // return this.encoding.charToGlyphIndex(c) !== null;
+    return this.encoding.charToGlyphIndex(c) !== 0; // 완벽하지 않음.
 };
 
 /**
